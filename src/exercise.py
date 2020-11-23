f8aa4fff62a21b1f7f1d24e2f9c0b995cd1c07db
rhysrawnson2101 <Rhysrawnson@gmail.com>
2020-11-23 15:08:35 +0000

    three numbers averaged

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,9 @@
 def main():
     #write your code below this line
+  number_one = int(input('choose first number:'))
+  number_two = int(input('choose second number:'))
+  number_three = int(input('choose thord number:'))
 
+  print((number_one + number_two + number_three)/3)
 if __name__ == '__main__':
     main()
